1ad9e5556caacd389fb758c0dd79d2d48e98a33c
devguru <ddguru777@gmail.com>
2020-01-17 11:04:00 -0400

    cache api result

--- src/actions/newsAction.js
+++ src/actions/newsAction.js
@@ -18,33 +18,50 @@ const onLoadNews = () => {
     try {
       dispatch(isLoading(true));
 
-      const result = await NewsAPI.onRetrieveNews();
+      let news = [];
+      let featuredNews = [];
 
-      dispatch(isLoading(false));
+      let bCached = false;
 
-      const news = [];
-      const featuredNews = [];
+      // If previous result has already been saved to browser, do not call NY API
+      if (localStorage.getItem('news') && localStorage.getItem('featuredNews')) {
+        news = JSON.parse(localStorage.getItem('news'));
+        featuredNews = JSON.parse(localStorage.getItem('featuredNews'));
 
-      // Parsiong json result from NYTimes API
-      for (let i = 0; i < result.response.docs.length; i++) {
-        const newsItem = {
-          key: i,
-          title: result.response.docs[i].headline.main,
-          pub_date: result.response.docs[i].pub_date,
-          summary: result.response.docs[i].snippet,
-          mainImage:
-            result.response.docs[i].multimedia.length === 0
-              ? ''
-              : `${c.SITE_URL}/${result.response.docs[i].multimedia[0].url}`,
-        };
-        news.push(newsItem);
-
-        if (newsItem.mainImage !== '' && featuredNews.length < 3) {
-          featuredNews.push(newsItem);
+        if (news.length !== 0 && featuredNews !== 0) {
+          bCached = true;
         }
       }
 
+      if (!bCached) {
+        const result = await NewsAPI.onRetrieveNews();
+
+        // Parsiong json result from NYTimes API
+        for (let i = 0; i < result.response.docs.length; i++) {
+          const newsItem = {
+            key: i,
+            title: result.response.docs[i].headline.main,
+            pub_date: result.response.docs[i].pub_date,
+            summary: result.response.docs[i].snippet,
+            mainImage:
+              result.response.docs[i].multimedia.length === 0
+                ? ''
+                : `${c.SITE_URL}/${result.response.docs[i].multimedia[0].url}`,
+          };
+          news.push(newsItem);
+
+          if (newsItem.mainImage !== '' && featuredNews.length < 3) {
+            featuredNews.push(newsItem);
+          }
+        }
+      }
+
+      // Cache api result
+      localStorage.setItem('news', JSON.stringify(news));
+      localStorage.setItem('featuredNews', JSON.stringify(featuredNews));
+
       dispatch(onLoadNewsSuccess(news, featuredNews));
+      dispatch(isLoading(false));
 
       return news;
     } catch (error) {
